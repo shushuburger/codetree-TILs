5d3883f5bb328ad1df078800a053e97de1277864
shushuburger <109223626+shushuburger@users.noreply.github.com>
2024-03-13 10:49:45 +0900

    Title: 달에서 무게 구하기, Time: 110ms, Memory: 24MB, Status: Passed - CodeTree

--- 240313/달에서 무게 구하기/weight-on-the-moon.py
+++ 240313/달에서 무게 구하기/weight-on-the-moon.py
@@ -1,6 +1,10 @@
-a = 33.567268
-print("%.4f" %a)
+# a = 33.567268
+# print("%.4f" %a)
 
-print("{0:.4f}".format(a))
+# print("{0:.4f}".format(a))
 
-print(f"{a:.4f}")
+# print(f"{a:.4f}")
+
+a,b = 13, 0.165
+
+print(f"{a} * {b:.6f} = {a*b:.6f}")
