6f5cc76ee58641c41f700275bebd85ea11ccd317
shushuburger <109223626+shushuburger@users.noreply.github.com>
2024-03-13 11:03:41 +0900

    Title: 변수 값 교체하기 3, Time: 122ms, Memory: 24MB, Status: Passed - CodeTree

--- 240313/변수 값 교체하기 3/replacing-variable-values-3.py
+++ 240313/변수 값 교체하기 3/replacing-variable-values-3.py
@@ -1,12 +1,16 @@
-a, b = 5, 3
+# a, b = 5, 3
 
-temp = a
-a = b
-b = temp
+# temp = a
+# a = b
+# b = temp
 
-print(f"A is {a} B is {b}")
+# print(f"A is {a} B is {b}")
 
-a, b = 5, 3
+# a, b = 5, 3
+# a, b = b, a
+
+# print(f"A is {a} B is {b}")
+
+a, b = 3, 5
 a, b = b, a
-
-print(f"A is {a} B is {b}")
+print(f"{a}\n{b}")
