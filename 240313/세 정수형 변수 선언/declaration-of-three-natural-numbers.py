d4e8aded3990237928b63355373fe3b94bbcd9db
shushuburger <109223626+shushuburger@users.noreply.github.com>
2024-03-13 10:44:14 +0900

    Title: 세 정수형 변수 선언, Time: 107ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240313/세 정수형 변수 선언/declaration-of-three-natural-numbers.py
@@ -0,0 +1,20 @@
+# a = 5
+# print("A is %d" %a)
+
+# b = "apple"
+# print("B is %s" %b)
+
+# print("A is %d and B is %s" %(a,b))
+
+# a, b = 5, "apple"
+
+# print("A is {}".format(a))
+# print("A is {0}".format(a))
+# print("A is {new_a}".format(new_a = a))
+
+# a,b = 5, "apple"
+# print(f"B is {b}")
+
+a, b, c = 7, 23, 30
+
+print(f"{a} + {b} = {c}")
